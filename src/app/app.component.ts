95b92f6bc4bcf7764f43614202f6249271011244
Thiago Veloso <thiago.pio2017@gmail.com>
2022-01-03 15:13:18 -0300

    initialized electron api implementation through ngx-electron

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,22 @@
 import { Component } from '@angular/core';
+import { ElectronService } from 'ngx-electron';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'electron-test';
+
+  constructor(public electronService: ElectronService) {
+    console.log(this.electronService.isElectronApp);
+  }
+
+  public minimizableWindow() {
+    if (this.electronService.isElectronApp) {
+      console.log(this.electronService.remote.BrowserWindow);
+      this.electronService.remote.BrowserWindow.getFocusedWindow()?.minimize();
+    }
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgxElectronModule } from 'ngx-electron';
 
 import { AppComponent } from './app.component';
 
@@ -8,7 +9,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxElectronModule
   ],
   providers: [],
   bootstrap: [AppComponent]
